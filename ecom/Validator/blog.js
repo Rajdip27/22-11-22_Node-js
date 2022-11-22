53dcbaa23bec40b920ecefe0a3f5e051a5be0835
Raj <srajdip920@gmail.com>
2022-11-22 12:56:31 +0600

    My Work

---
+++ ecom/Validator/blog.js
@@ -0,0 +1,15 @@
+const { check } = require('express-validator');
+
+exports.store=[
+    check('title', "Invalid title").not().isEmpty().trim(),
+    check('details', "Invalid details").not().isEmpty().trim(),
+    check('slug','Slug Invalid').not().isEmpty().trim(),
+    check('image', "Invalid image").not().isEmpty().trim(),
+]
+
+exports.update=[
+    check('title', "Invalid title").not().isEmpty().trim(),
+    check('details', "Invalid details").not().isEmpty().trim(),
+    check('slug','Slug Invalid').not().isEmpty().trim(),
+    check('image', "Invalid image").not().isEmpty().trim(),
+]
---
+++ ecom/controlers/blogControler.js
@@ -0,0 +1,62 @@
+const{validationResult}=require('express-validator');
+const BlogModel=require('../models/blog');
+module.exports={
+    index:(req, res, next)=> {
+        // blog list
+        
+        res.render('backend/blog/index', { title: 'Blogs',layout:"backend/layout" });
+    },
+    create:(req, res, next)=> {
+        // blog list
+        res.render('backend/blog/create', { title: 'Blog Create', layout: 'backend/layout' })
+    },
+    edit:(req, res, next)=> {
+        // blog list
+        res.render('index', { title: 'blogs' });
+    },
+    show:(req, res, next)=> {
+
+        // blog list
+        // BlogModel.find((err,docs)=>{
+        //     if(err){
+        //         return res.json({error:"Something went wrong!"+err})
+        //     }
+        //     return res.json({blogs:docs});
+        // });
+        res.render('index', { title: 'blogs' });
+    },
+    delete:(req, res, next)=> {
+        // blog list
+        res.render('index', { title: 'blogs' });
+    },
+    update:(req, res, next)=> {
+        // blog list
+        res.render('index', { title: 'blogs' });
+    },
+    store:(req, res, next)=> {
+
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.json({errors:errors.mapped()});
+        }
+
+        const blog=new BlogModel({
+            title:req.body.title,
+            slug:req.body.slug,
+            details:req.body.details,
+            image:req.body.image
+        });
+
+        blog.save((err,newBlog)=>{
+            if(err){
+              return res.json({error:"Something went wrong!"+err})
+            }
+            return res.json({blog:newBlog});
+        });
+
+
+
+        // return res.json(req.body);
+        // res.render('index', { title: 'blogs' });
+    }
+}
